830fd71e9ec556edebaa8ace5186e3d154972c18
Norbix022 <norbert.anton@icloud.com>
2026-06-29 17:51:47 +0200

    Update Sphinx configuration for documentation

--- source/conf.py
+++ source/conf.py
@@ -1,141 +1,73 @@
 # Configuration file for the Sphinx documentation builder.
 
-
-
 import os
-
 import sys
 
-
-
 # Dodanie ścieżki do obecnego katalogu (source/) ORAZ nadrzędnego (główny folder projektu)
-
-# Dzięki temu autodoc znajdzie pliki .py niezależnie od tego, gdzie dokładnie je zapisałeś.
-
 sys.path.insert(0, os.path.abspath('.'))
-
 sys.path.insert(0, os.path.abspath('..'))
 
-
-
 # -- Project information -----------------------------------------------------
-
-project = 'Sprawozdanie-z-laboratoriów'
-
+project = 'Sprawozdanie-z-laboratorium'
 copyright = '2026, Norbert Antonovitch'
-
 author = 'Norbert Antonovitch'
 
-
-
 # -- General configuration ---------------------------------------------------
+# Zostawiamy język polski, żeby tłumaczył "Chapter" na "Rozdział"
+language = 'pl'
 
 # Dodanie rozszerzenia autodoc
-
 extensions = [
-
     'sphinx.ext.autodoc'
-
 ]
 
-
-
 templates_path = ['_templates']
-
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
-
 html_theme = 'alabaster'
-
 html_static_path = ['_static']
 
-
-
 # -- Options for LaTeX output ------------------------------------------------
-
-# Optymalizacja generowania PDF - eliminacja pustych stron między rozdziałami
-
 latex_elements = {
-
     'papersize': 'a4paper',
-
     'pointsize': '11pt',
-
+    # PRZYWRACAMY STYL BJARNE (Kreski i wyrównanie do prawej)
     'fncychap': '\\usepackage[Bjarne]{fncychap}',
-
     'extraclassoptions': 'openany,oneside',
-
     'printindex': '',
-
     'preamble': r'''
-
 \usepackage{babel}
-
 \usepackage{graphicx}
-
 \usepackage{hyperref}
-
 \setcounter{tocdepth}{2}
-
 \raggedbottom
 
-
-
 % TWARDY RESET INDEKSU - zmusza LaTeXa do zignorowania komendy generującej spis
-
 \renewcommand{\printindex}{}
 
-
-
 % Zmniejszenie przestrzeni przed nagłówkami
-
 \setlength{\parskip}{0pt plus 1pt}
-
 \setlength{\parindent}{0pt}
 
-
-
 % Zwiększenie wysokości headera
-
 \setlength{\headheight}{14.49998pt}
-
 ''',
-
     'sphinxsetup': 'hmargin={0.7in,0.7in}, vmargin={0.7in,0.7in}, verbatimwithframe=false',
-
 }
 
 latex_documents = [
-
     (
-
         'index', 
-
         '276209-bazy-danych-sprawozdanie.tex', 
-
         'Sprawozdanie z Laboratorium: Bazy Danych',
-
-        'Norbert Antonovitch (276209)',  # Combined authors into one string
-
-        'manual'              # This is now correctly the documentclass
-
+        'Norbert Antonovitch (276209)',
+        'manual'
     ),
-
 ]
 
-
-
 latex_show_urls = 'footnote'
-
 latex_show_pagerefs = False
-
 latex_max_embed_pages = 0
-
-
-
-# KLUCZOWE: Wyłącza generowanie bzdurnego "Python Module Index" w PDF i spisie treści
 
 latex_domain_indices = False
